2f449adeb25807ea755057c6d97ffa2810165d81
Sergio Behrends <sergio@behrends.com.ar>
2018-03-14 21:09:54 -0300

    Cache some pages

--- next.config.js
+++ next.config.js
@@ -4,11 +4,27 @@ module.exports = withOffline({
   workboxOpts: {
     runtimeCaching: [
       {
-        urlPattern: /.*(png|jpg|gif|svg)/,
+        urlPattern: /.*(png|jpg|gif|svg|jpeg)/,
         handler: 'cacheFirst',
       },
       {
         urlPattern: /https:\/\/api.canillitapp.com\/.+/,
+        handler: 'networkFirst',
+      },
+      {
+        urlPattern: /\/keyword\/(.*)\/(.*)/,
+        handler: 'networkFirst',
+      },
+      {
+        urlPattern: /\/article\/(.*)/,
+        handler: 'networkFirst',
+      },
+      {
+        urlPattern: /\/(\d*)-(\d*)-(\d*)/,
+        handler: 'networkFirst',
+      },
+      {
+        urlPattern: /\//,
         handler: 'networkFirst',
       },
     ],
