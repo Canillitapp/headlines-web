76a0803d4ccfb212634135096de4a6b279422488
Sergio Behrends <sergio.behrends@mulesoft.com>
2019-05-26 21:44:55 -0300

    Added HTTPS server for local development. Required for Facebook login

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const config = require('./config')
 const Server = require('./server')
+const Winston = require('./server/lib/logger')()
 
-Server.start()
+if (config.env === 'development' && !config.noSSL) {
+  // Start a secure HTTPS server with an selfsigned certificate
+  // This is useful for local development
+  // Set NO_SSL environment variable to force no SSL
+  const pem = require('pem')
+  Winston.info(`Starting secure HTTPS server`)
+  pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+    if (err) {
+      throw err
+    }
+    Server.start({ ssl: keys })
+  })
+} else {
+  // Start HTTP server
+  Winston.info(`Starting HTTP server`)
+  Server.start()
+}
