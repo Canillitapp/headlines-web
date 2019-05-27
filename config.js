cb4216eca4d62168bce915890cfd4ed66c754c38
Sergio Behrends <sergio.behrends@mulesoft.com>
2019-05-26 21:44:55 -0300

    Support local HTTPS

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
-export default {
+const env = process.env.NODE_ENV || 'development'
+const isDev = env !== 'production'
+
+module.exports = {
   baseApi: process.env.API_URL || 'https://api-stg.dokku.canillitapp.com',
+  env,
+  isDev,
+  isProd: !isDev,
+  noSSL: process.env.NO_SSL || false,
 }
