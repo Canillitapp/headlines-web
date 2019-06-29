71e4c8e0441a0be346b0a146f23d79e97151d21d
Sergio Behrends <sergio.behrends@mulesoft.com>
2019-06-29 15:18:16 -0300

    Add reaction

--- shared/components/ReactionGroup.js
+++ shared/components/ReactionGroup.js
@@ -92,15 +92,17 @@ function ReactionGroup({ id, reactions }) {
           }}
         />
       ))}
-      <Reaction emoji="➕" amount={null} onClick={handleReactionModalOpen} />
+      <Reaction
+        emoji={
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20" style={{ fill: '#c3c3c3' }}><path d="M8.55,3A8.44,8.44,0,1,0,17,11.47,8.43,8.43,0,0,0,8.55,3Zm0,15.5a7.07,7.07,0,1,1,7.06-7.07A7.07,7.07,0,0,1,8.55,18.54Z" /><path d="M9.55,14.9a3.7,3.7,0,0,0,3.56-2.73.78.78,0,0,0-.77-1H6.75a.78.78,0,0,0-.77,1A3.71,3.71,0,0,0,9.55,14.9Z" /><path d="M19.4,2.35H17.63V.58a.5.5,0,0,0-.14-.35.48.48,0,0,0-.34-.14h-.33a.48.48,0,0,0-.48.48V2.35H14.55a.48.48,0,0,0-.46.48v.35a.49.49,0,0,0,.46.46h1.76V5.4a.55.55,0,0,0,.14.37.5.5,0,0,0,.37.14h.33a.49.49,0,0,0,.49-.48V3.64H19.4a.49.49,0,0,0,.49-.46V2.83A.48.48,0,0,0,19.4,2.35Z" /><circle cx="6.73" cy="8.7" r="1.29" /><circle cx="12.65" cy="8.7" r="1.29" /></svg>
+      }
+        amount={null}
+        onClick={handleReactionModalOpen}
+      />
       <style jsx>{`
         .ReactionGroup {
           margin-top: 10px;
         }
-
-        // .ReactionGroup :global(div) {
-        //   margin-right: 5px;
-        // }
       `}
       </style>
     </div>
