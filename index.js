5fc7a3451485cf3c7828faf1fac878618be1fd5a
Sergio Behrends <sergio@behrends.com.ar>
2019-07-14 23:18:09 -0300

    Upgrade to Next JS 9 (#34)
    * Upgrade NextJS
    * Re use created cert

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const config = require('./config')
 const Server = require('./server')
 const Winston = require('./server/lib/logger')()
@@ -10,10 +11,25 @@ if (config.env === 'development' && !config.noSSL) {
   // eslint-disable-next-line global-require, import/no-extraneous-dependencies
   const pem = require('pem')
   Winston.info('Starting secure HTTPS server')
+
+  const certsTempFile = './.certs.tmp'
+  // If exists, use self signed stored certs
+  if (fs.existsSync(certsTempFile)) {
+    try {
+      const keys = JSON.parse(fs.readFileSync(certsTempFile))
+      Server.start({ ssl: keys })
+      return
+    } catch (err) {
+      throw err
+    }
+  }
+
+  // Create temporal self signed certs for local development
   pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
     if (err) {
       throw err
     }
+    fs.writeFileSync(certsTempFile, JSON.stringify(keys))
     Server.start({ ssl: keys })
   })
 } else {
